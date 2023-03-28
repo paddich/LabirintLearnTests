d47e080901525f24cb6753146b10e655f631616c
paddich <skachkovaa.work@gmail.com>
2023-03-28 18:01:35 +0500

    какие-то измнения

--- LabirintLearnTests/UnitTest1.cs
+++ LabirintLearnTests/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace LabirintLearnTests
 
         private readonly By _mainSearchField = By.XPath("//input[@id=\"search-field\"]");
         private readonly By _mainSearchButton = By.XPath("//button[@form=\"searchform\"]");
-        //private readonly By _mainSearchResult = By.XPath("//h1[@class=\"index-top-title\"]");
+        private readonly By _mainSearchResult = By.XPath("//h1[@class=\"index-top-title\"]");
 
         private readonly By _findedBookIntoBrasketButton = By.XPath("//a[@data-idtov=\"842998\"]");
         private readonly By _brasketButton = By.XPath("//span[text()=\"Корзина\"]");
@@ -21,7 +21,9 @@ namespace LabirintLearnTests
         private readonly By _brasketRestore = By.XPath("//a[@class=\"b-link-popup g-alttext-deepblue\"]");
 
         private const string _searchRequest = "Чак Паланик";
-        //private const string _expextedSearchResult = "Все, что мы нашли в Лабиринте по запросу «" + _searchRequest + "»";
+        private const string _expextedSearchResult = "Все, что мы нашли в Лабиринте по запросу «" + _searchRequest + "»";
+
+        private readonly By _footerVkLink = By.XPath("//a[@data-event-content=\"ВКонтакте\"]");
 
         [SetUp]
         public void Setup()
@@ -31,26 +33,26 @@ namespace LabirintLearnTests
             driver.Manage().Window.Maximize();
         }
 
-        //[Test]
-        //public void MainSearchTest()
-        //{
-        //    var searchFieldInput = driver.FindElement(_mainSearchField);
-        //    searchFieldInput.SendKeys(_searchRequest);
+        [Test]
+        public void MainSearchTest()
+        {
+            var searchFieldInput = driver.FindElement(_mainSearchField);
+            searchFieldInput.SendKeys(_searchRequest);
 
-        //    Thread.Sleep(700);
+            Thread.Sleep(700);
 
-        //    var searchStart = driver.FindElement(_mainSearchButton);
-        //    searchStart.Click();
+            var searchStart = driver.FindElement(_mainSearchButton);
+            searchStart.Click();
 
-        //    Thread.Sleep(700);
+            Thread.Sleep(700);
 
-        //    string actualSearchResult = driver.FindElement(_mainSearchResult).Text;
+            string actualSearchResult = driver.FindElement(_mainSearchResult).Text;
 
-        //    Assert.AreEqual(_expextedSearchResult, actualSearchResult);
-        //}
+            Assert.AreEqual(_expextedSearchResult, actualSearchResult);
+        }
 
         [Test]
-        public void BrasketInputTest()
+        public void BrasketTest()
         {
             var searchFieldInput = driver.FindElement(_mainSearchField);
             searchFieldInput.SendKeys(_searchRequest);
@@ -92,6 +94,15 @@ namespace LabirintLearnTests
             brasketRestore.Click();
 
             // Конец теста восстановления корзины
+            Assert.Pass();
+        }
+
+        [Test]
+        public void FooterTest()
+        {   
+            var footerVK = driver.FindElement(_footerVkLink);
+            footerVK.Click();
+
             Assert.Pass();
         }
 
